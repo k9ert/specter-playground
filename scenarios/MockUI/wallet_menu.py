b75112db8d5be1fd96a0edb5bfad157034b5dd21
maggo83 <marco.kruse@gmx.de>
2025-10-23 10:01:11 +0200

    Bug fix: added missing "," in WalletMenu

--- scenarios/MockUI/wallet_menu.py
+++ scenarios/MockUI/wallet_menu.py
@@ -25,7 +25,7 @@ def WalletMenu(parent, *args, **kwargs):
         ("Rename Wallet", "rename_wallet"),
         ("Delete Wallet", "delete_wallet"),
         ("Connect/Export", None),
-        ("Connect SW Wallet", "connect_sw_wallet")
+        ("Connect SW Wallet", "connect_sw_wallet"),
         ("Export Data", "export_wallet")
     ]
 
