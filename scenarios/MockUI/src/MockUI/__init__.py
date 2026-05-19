e37716f4f2f37dfbb18b7d43a09694d848832e57
maggo83 <marco.kruse@gmx.de>
2026-05-19 11:33:51 +0200

    (refactor) R9: trim re-exports in basic/__init__.py and MockUI/__init__.py
    Both package init files re-exported a much larger surface than callers actually consumed. Restrict the re-exports (and __all__) to the names actually imported from outside the package: just the names used by sibling packages (seed/, wallet/, device/) for basic, and only SpecterGui, DeviceState, Wallet for the top-level MockUI package. Multi-line import groups make the remaining surface easy to read.

--- scenarios/MockUI/src/MockUI/__init__.py
+++ scenarios/MockUI/src/MockUI/__init__.py
@@ -1,63 +1,9 @@
 # MockUI/__init__.py
-from .basic import BTN_HEIGHT, BTN_WIDTH, MENU_PCT, SMALL_PAD, SWITCH_HEIGHT, SWITCH_WIDTH, STATUS_BTN_HEIGHT, STATUS_BTN_WIDTH, GREEN, ORANGE, RED
-from .basic import MainMenu, LockedMenu, ActionScreen, GenericMenu
 from .basic import SpecterGui
-from .tour import UIExplainer, GuidedTour
-
-from .stubs import UIState, DeviceState, Wallet, Seed
-
-from .wallet import (
-    WalletMenu,
-    AddWalletMenu,
-    ConnectWalletsMenu,
-)
-from .seed import (
-    SeedPhraseMenu,
-    GenerateSeedMenu,
-    PassphraseMenu,
-)
-
-from .device import (
-    SecuritySettingsMenu,
-    FirmwareMenu,
-    InterfacesMenu,
-    BackupsMenu,
-    SecurityFeaturesMenu,
-    StorageMenu,
-    SettingsMenu,
-    PreferencesMenu,
-)
-
-from .tour import GuidedTour
+from .stubs import DeviceState, Wallet
 
 __all__ = [
-    "BTN_HEIGHT", "BTN_WIDTH",
-    "MENU_PCT",
-    "SMALL_PAD",
-    "SWITCH_HEIGHT", "SWITCH_WIDTH",
-    "STATUS_BTN_HEIGHT", "STATUS_BTN_WIDTH",
-    "GREEN", "ORANGE", "RED",
-    "MainMenu",
-    "WalletMenu",
-    "SecuritySettingsMenu",
+    "SpecterGui",
     "DeviceState",
     "Wallet",
-    "Seed",
-    "ActionScreen",
-    "UIState",
-    "SeedPhraseMenu",
-    "SecurityFeaturesMenu",
-    "InterfacesMenu",
-    "BackupsMenu",
-    "FirmwareMenu",
-    "ConnectWalletsMenu",
-    "AddWalletMenu",
-    "LockedMenu",
-    "GenerateSeedMenu",
-    "StorageMenu",
-    "SettingsMenu",
-    "PassphraseMenu",
-    "SpecterGui",
-    "UIExplainer",
-    "GuidedTour",
 ]
--- scenarios/MockUI/src/MockUI/basic/__init__.py
+++ scenarios/MockUI/src/MockUI/basic/__init__.py
@@ -1,58 +1,34 @@
-from .ui_consts import BTN_HEIGHT, BTN_WIDTH, BACK_BTN_HEIGHT, BACK_BTN_WIDTH, MENU_PCT, SMALL_PAD, PAD, BIG_PAD, SWITCH_HEIGHT, SWITCH_WIDTH, STATUS_BTN_HEIGHT, STATUS_BTN_WIDTH, CARD_H, STATUS_BAR_PCT, CONTENT_PCT, BTC_ICON_WIDTH, BTC_ICON_ZOOM, MENU_TITLE_FONT_SIZE, MENU_ITEM_FONT_SIZE, GREEN, ORANGE, RED, WHITE, GREY, BLACK, GREEN_HEX, ORANGE_HEX, RED_HEX, WHITE_HEX, GREY_HEX, BLACK_HEX, TITLE_ROW_HEIGHT, TITLE_PADDING, MODAL_WIDTH_PCT, MODAL_HEIGHT_PCT, EXPLAINER_WIDTH_PCT, EXPLAINER_HEIGHT_PCT, PIN_BTN_WIDTH, PIN_BTN_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT, DEFAULT_MODAL_BG_OPA, MAX_HISTORY_DEPTH, TITLE_TA_WIDTH, ANIM_MS_HORIZONTAL, ANIM_MS_VERTICAL, TITLE_FONT, TEXT_FONT, SMALL_TEXT_FONT, DROPUP_DIVIDER_OPA, BATTERY_WIDTH, FINGERPRINT_LBL_WIDTH
-from .ui_utils import to_lv_color, to_hex_str
+from .ui_consts import (
+    BTN_HEIGHT, BTN_WIDTH,
+    SMALL_PAD,
+    SWITCH_HEIGHT, SWITCH_WIDTH,
+    STATUS_BTN_HEIGHT, STATUS_BTN_WIDTH,
+    MENU_PCT,
+    TITLE_ROW_HEIGHT,
+    GREEN, ORANGE, RED,
+    ORANGE_HEX, RED_HEX, WHITE_HEX, GREY_HEX,
+)
 from .titled_screen import TitledScreen
 from .main_menu import MainMenu
 from .locked_menu import LockedMenu
-from .context_bar import ContextBar
-
-from .navigation_bar import NavigationBar
-from .dropup import SeedDropUp, WalletDropUp, DropUpState
 from .action_screen import ActionScreen
 from .menu import GenericMenu
-from .widgets.modal_overlay import ModalOverlay
-from .widgets.action_modal import ActionModal
 from .switch_add_menu import SwitchAddMenu
 from .specter_gui import SpecterGui
-from .app_screen import AppScreen
-from .symbol_lib import BTC_ICONS
-from .widgets import Btn, flex_col, flex_row, dialog_card
-from .widgets import body_label, section_header, form_label
-from .widgets import title_textarea, form_textarea, ACCEPTED_CHARS
-from .widgets import MenuItem
-from .widgets import make_icon, set_visible
-from .animations import GUIAnimations, slide_x, slide_y
 
-__all__ = ["BTN_HEIGHT", "BTN_WIDTH", "BACK_BTN_HEIGHT", "BACK_BTN_WIDTH",
-           "SCREEN_WIDTH", "SCREEN_HEIGHT",
-           "MENU_PCT",
-           "SMALL_PAD", "PAD", "BIG_PAD",
-           "STATUS_BTN_HEIGHT", "STATUS_BTN_WIDTH", "CARD_H",
-           "TITLE_ROW_HEIGHT", "TITLE_PADDING", "TITLE_TA_WIDTH",
-           "MODAL_WIDTH_PCT", "MODAL_HEIGHT_PCT", "DEFAULT_MODAL_BG_OPA",
-           "EXPLAINER_WIDTH_PCT", "EXPLAINER_HEIGHT_PCT",
-           "DROPUP_DIVIDER_OPA",
-           "BATTERY_WIDTH",
-           "SWITCH_HEIGHT", "SWITCH_WIDTH",
-           "STATUS_BTN_HEIGHT", "STATUS_BTN_WIDTH",
-           "PIN_BTN_WIDTH", "PIN_BTN_HEIGHT",
-           "STATUS_BAR_PCT", "CONTENT_PCT",
-           "BTC_ICON_WIDTH", "BTC_ICON_ZOOM",
-           "MENU_TITLE_FONT_SIZE", "MENU_ITEM_FONT_SIZE", "TITLE_FONT", "TEXT_FONT", "SMALL_TEXT_FONT",
-           "FINGERPRINT_LBL_WIDTH",
-           "GREEN", "ORANGE", "RED", "WHITE", "GREY", "BLACK",
-           "GREEN_HEX", "ORANGE_HEX", "RED_HEX", "WHITE_HEX", "GREY_HEX", "BLACK_HEX",
-           "MainMenu", "LockedMenu", "ContextBar", "NavigationBar", "SeedDropUp", "WalletDropUp", "DropUpState", "ActionScreen",
-           "GenericMenu", "TitledScreen", "ModalOverlay", "ActionModal", "SpecterGui", "AppScreen",
-           "BTC_ICONS", "SwitchAddMenu",
-           "MAX_HISTORY_DEPTH",
-           "ANIM_MS_HORIZONTAL", "ANIM_MS_VERTICAL",
-           "to_lv_color", "to_hex_str",
-           # widgets/
-           "Btn",
-           "flex_col", "flex_row", "dialog_card",
-           "body_label", "section_header", "form_label",
-           "title_textarea", "form_textarea", "ACCEPTED_CHARS",
-           "MenuItem",
-           "make_icon", "set_visible",
-           "GUIAnimations", "slide_x", "slide_y",
-        ]
+__all__ = [
+    # ui_consts re-exports used outside basic/
+    "BTN_HEIGHT", "BTN_WIDTH",
+    "SMALL_PAD",
+    "SWITCH_HEIGHT", "SWITCH_WIDTH",
+    "STATUS_BTN_HEIGHT", "STATUS_BTN_WIDTH",
+    "MENU_PCT",
+    "TITLE_ROW_HEIGHT",
+    "GREEN", "ORANGE", "RED",
+    "ORANGE_HEX", "RED_HEX", "WHITE_HEX", "GREY_HEX",
+    # classes used outside basic/
+    "TitledScreen",
+    "MainMenu", "LockedMenu",
+    "ActionScreen", "GenericMenu", "SwitchAddMenu",
+    "SpecterGui",
+]
