8a8009a4f3f1eb0bfa2cd52467d68bcc2bf3e0bb
maggo83 <marco.kruse@gmx.de>
2026-06-13 15:08:32 +0200

    (fix) Battery icon background visibility
    USER states interferred with DISABLE state -> level_bg remained visible all the time.
    added helper function to first remove all user+disbaled state and then only set the one that we want/need.

--- scenarios/MockUI/src/MockUI/basic/widgets/battery.py
+++ scenarios/MockUI/src/MockUI/basic/widgets/battery.py
@@ -5,7 +5,7 @@ from ..symbol_lib import BTC_ICONS
 from ..theming import apply_style
 
 ALL_STATES = (
-    lv.STATE.USER_1 | lv.STATE.USER_2 | lv.STATE.USER_3 | lv.STATE.USER_4
+    lv.STATE.USER_1 | lv.STATE.USER_2 | lv.STATE.USER_3 | lv.STATE.USER_4 | lv.STATE.DISABLED
 )
 
 level_states = {
@@ -13,7 +13,13 @@ level_states = {
     "high": lv.STATE.USER_2,
     "mid":  lv.STATE.USER_3,
     "low":  lv.STATE.USER_4,
+    "off":  lv.STATE.DISABLED,
 }
+
+def set_level(obj, level):
+        obj.set_state(ALL_STATES, False)
+        obj.set_state(level_states[level], True)
+
 class Battery(lv.obj):
     LEVELS = [
         (95, BTC_ICONS.BATTERY_FULL_OUTLINE,   "full"),
@@ -32,20 +38,20 @@ class Battery(lv.obj):
         self.level_bg = make_icon(self, BTC_ICONS.BATTERY_EMPTY)
         set_align(self.level_bg, lv.ALIGN.CENTER)
         apply_style(self.level_bg, "WIDGET.BATTERY")
-        apply_style(self.level_bg, "APPEARANCE.INVISIBLE", lv.STATE.DISABLED)
         apply_style(self.level_bg, "FG.DEFAULT", lv.STATE.USER_1)
         apply_style(self.level_bg, "FG.SUCCESS", lv.STATE.USER_2)
         apply_style(self.level_bg, "FG.WARNING", lv.STATE.USER_3)
         apply_style(self.level_bg, "FG.DANGER",  lv.STATE.USER_4)
+        apply_style(self.level_bg, "APPEARANCE.INVISIBLE", lv.STATE.DISABLED)        
 
         self.level = make_icon(self, BTC_ICONS.BATTERY_FULL_OUTLINE)
         set_align(self.level, lv.ALIGN.CENTER)
         apply_style(self.level, "WIDGET.BATTERY")
-        apply_style(self.level, "APPEARANCE.INVISIBLE", lv.STATE.DISABLED)
         apply_style(self.level, "FG.DEFAULT", lv.STATE.USER_1)
         apply_style(self.level, "FG.SUCCESS", lv.STATE.USER_2)
         apply_style(self.level, "FG.WARNING", lv.STATE.USER_3)
         apply_style(self.level, "FG.DANGER",  lv.STATE.USER_4)
+        apply_style(self.level, "APPEARANCE.INVISIBLE", lv.STATE.DISABLED)
 
         self.level_ol = make_icon(self, BTC_ICONS.BATTERY_EMPTY_OUTLINE)
         set_align(self.level_ol, lv.ALIGN.CENTER)
@@ -65,13 +71,10 @@ class Battery(lv.obj):
         if charging is not None:
             self.charging = charging
         if self.value is None:
-            self.level_bg.set_state(lv.STATE.DISABLED, True)
-            self.level.set_state(lv.STATE.DISABLED, True)
-            self.charge.set_state(lv.STATE.DISABLED, True)
+            set_level(self.level_bg, "off")
+            set_level(self.level, "off")
+            set_level(self.charge, "off")
             return
-
-        self.level_bg.set_state(lv.STATE.DISABLED, False)
-        self.level.set_state(lv.STATE.DISABLED, False)
 
         for v, level_icon, level in self.LEVELS:
             if self.value >= v:
@@ -80,44 +83,40 @@ class Battery(lv.obj):
                     # when charging user has taken appropriate action -> do not highlight
                     # low battery levels too much anymore -> just set levels and 
                     # not background to level color. Make background invisible
-                    self.level_bg.set_state(lv.STATE.DISABLED, True)
+                    set_level(self.level_bg, "off")
                     apply_icon(self.level, level_icon)
-                    self.level.set_state(ALL_STATES, False)
-                    self.level.set_state(level_states[level], True)
+                    set_level(self.level, level)
                 else:
                     if level == "full":
                         # Full battery, no need to draw user attention to this icon.
                         # Hence use full neutral coloring
                         apply_icon(self.level_bg, BTC_ICONS.BATTERY_EMPTY)
-                        self.level_bg.set_state(ALL_STATES, False)
-                        self.level_bg.set_state(level_states[level], True)
-                        self.level.set_state(lv.STATE.DISABLED, True)
+                        set_level(self.level_bg, level)
+
+                        set_level(self.level, "off")
                     elif level == "high":
                         # Almost full battery, no need to draw user attention to this icon.
                         # Hence only color levels in green (and not whole background)
-                        self.level_bg.set_state(lv.STATE.DISABLED, True)
+                        set_level(self.level_bg, "off")
 
                         apply_icon(self.level, level_icon)
-                        self.level.set_state(ALL_STATES, False)
-                        self.level.set_state(level_states[level], True)
+                        set_level(self.level, level)
                     else:
                         # Low battery, important to draw user attention -> color whole icon
                         # background with level color
                         apply_icon(self.level_bg, BTC_ICONS.BATTERY_EMPTY)
-                        self.level_bg.set_state(ALL_STATES, False)
-                        self.level_bg.set_state(level_states[level], True)
+                        set_level(self.level_bg, level)
 
                         apply_icon(self.level, level_icon)
-                        self.level.set_state(ALL_STATES, False)
-                        self.level.set_state(level_states["full"], True)
+                        set_level(self.level, "full")
 
                 #always draw outline in white to have clear border and better visibility on different backgrounds
                 apply_icon(self.level_ol, BTC_ICONS.BATTERY_EMPTY_OUTLINE)
 
                 # Charging state is important to show user has taken appropriate action -> highlight with charging icon
                 if self.charging:
-                    self.charge.set_state(lv.STATE.DISABLED, False)
+                    set_level(self.charge, "full")
                 else:
-                    self.charge.set_state(lv.STATE.DISABLED, True)
+                    set_level(self.charge, "off")
 
                 break
