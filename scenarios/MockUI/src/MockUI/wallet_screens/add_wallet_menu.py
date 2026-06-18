47efbc61bdaa698ae376e2e6d88294206573cd18
maggo83 <marco.kruse@gmx.de>
2026-06-18 15:37:45 +0200

    (feat) add_wallet: add direct link to enable interfaces + highlight best option

--- scenarios/MockUI/src/MockUI/wallet_screens/add_wallet_menu.py
+++ scenarios/MockUI/src/MockUI/wallet_screens/add_wallet_menu.py
@@ -14,16 +14,36 @@ class AddWalletMenu(GenericMenu):
     def get_menu_items(self, t, state):
         menu_items = []
 
+        QR_enabled = state.QR_enabled()
+        SD_detected = state.SD_detected()
+        none_ready = not (QR_enabled or SD_detected)
+        both_ready = QR_enabled and SD_detected
+
+        #prefer SD over QR if detected, assuming user put it in intentionally
+        Interfaces_modifier = "Highlight" if none_ready else None
+        SD_modifier = "Highlight" if SD_detected else None
+        QR_modifier = "Highlight" if QR_enabled and not SD_detected else None
+
         # Import section
         menu_items.append(MenuItem(text=t("ADD_WALLET_IMPORT_FROM")))
 
-        if state.QR_enabled():
-            menu_items.append(MenuItem(BTC_ICONS.SCAN, t("HARDWARE_QR_CODE"), "import_from_qr"))
+        if QR_enabled:
+            menu_items.append(MenuItem(BTC_ICONS.QR_CODE, t("HARDWARE_QR_CODE"),
+                                        target="import_from_qr",
+                                        modifier=QR_modifier))
 
-        if state.SD_detected():
-            menu_items.append(MenuItem(BTC_ICONS.SD_CARD, t("HARDWARE_SD_CARD"), "import_from_sd"))
+        if SD_detected:
+            menu_items.append(MenuItem(BTC_ICONS.SD_CARD, t("HARDWARE_SD_CARD"),
+                                        target="import_from_sd", 
+                                        modifier=SD_modifier))
 
-        # Customize section
+        if not both_ready:            
+            menu_items.append(MenuItem(BTC_ICONS.FLIP_HORIZONTAL, text=t("ADD_WALLET_ENABLE_INTERFACES"),
+                                       target="interfaces", 
+                                       modifier=Interfaces_modifier))
+
+
+        # TODO: Customize section [DUMMY DURING DEVELOPMENT, remove once hw in integrated]
         menu_items += [
             MenuItem(text=t("ADD_WALLET_CUSTOMIZE")),
             MenuItem(BTC_ICONS.CONSOLE, t("ADD_WALLET_CREATE_CUSTOM"), "create_custom_wallet", is_submenu=True),
